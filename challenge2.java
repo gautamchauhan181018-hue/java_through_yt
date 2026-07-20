ad0eed69d2255a4a1043a83525c4e9f3e99dc0b9
gautam kumar <gautamchauhan181018@gmail.com>
2026-07-20 17:27:21 +0530

    challenge2

---
+++ challenge2.java
@@ -0,0 +1,11 @@
+//show the following pattern 
+
+public class challenge2{
+
+    public static void main(String args[]){
+
+        System.out.println("pattern 1:\n* \n* *\n* * *\n* * * *\n* * * *\n* * * * * ");
+        System.out.println("\npattern 2:\n* * * * *\n* * * *\n* * *\n* *\n*");
+        System.out.print("pattern 3:\n         *\n       * *\n    * * *\n  * * * *\n* * * * * ");
+    }
+}
