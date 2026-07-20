6697d5c61e7145c2b6f8356bb9b923b4bc222ef4
gautam kumar <gautamchauhan181018@gmail.com>
2026-07-20 17:56:02 +0530

    challenge 4

---
+++ challenge4.java
@@ -0,0 +1,18 @@
+//write a java program to add two number?
+
+import java.util.Scanner;
+
+public class challenge4{
+
+    public static void main(String args[]){
+    Scanner scn = new Scanner(System.in);
+
+    System.out.print("enter a first number:");
+    int a = scn.nextInt();
+
+    System.out.print("enter a second number:");
+    int b = scn.nextInt();
+
+    System.out.print(a + b);    
+    }
+}
