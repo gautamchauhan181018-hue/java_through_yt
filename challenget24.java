a05b0f48b5ad062f194d179e7e0c04db025c6eba
gautam kumar <gautamchauhan181018@gmail.com>
2026-08-10 19:40:35 +0530

    challenge24

---
+++ challenget24.java
@@ -0,0 +1,14 @@
+//write a java program for right shift operator?
+
+import java.util.Scanner;
+public class challenget24{
+
+    public static void main(String args[]){
+    Scanner scn = new Scanner(System.in);
+
+    System.out.print("enter the value of a: ");
+    int a = scn.nextInt();
+
+    System.out.print("right shift = "+ (a>>1)); 
+    }
+}
